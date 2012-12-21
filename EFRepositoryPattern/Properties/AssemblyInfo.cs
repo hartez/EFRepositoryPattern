f2038aa94a2df8c9a44c52059c9f6315ce825ec2
E.Z. Hart <hartez@gmail.com>
2012-12-21 14:21:02 -0700

    Bumped version

--- EFRepositoryPattern/Properties/AssemblyInfo.cs
+++ EFRepositoryPattern/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Traceur LLC")]
 [assembly: AssemblyProduct("EFRepositoryPattern")]
-[assembly: AssemblyCopyright("Copyright © 2011 Traceur LLC")]
+[assembly: AssemblyCopyright("Copyright © 2012 E.Z. Hart and Traceur LLC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
